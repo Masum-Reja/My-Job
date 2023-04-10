f8c0cde6eea2fc4be33361ffee8475a9a7915565
Masum-Reja <masum15-13347@diu.edu.bd>
2023-04-10 16:50:01 +0600

    setup router initially

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+ const loader=useLoaderData
+const Home = () => {
+  return (
+    <div>
+     <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
+
---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Components/Home'
+import Statistics from './Components/Statistics'
+import Blog from './Components/Blog'
+import ErrorPage from './Components/ErrorPage'
+import LoadingSpinner from './Components/LoadingSpinner'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "",
+        element: <Home></Home>,
+      },
+      {
+        path: "statistics",
+        element: <Statistics></Statistics>,
+      },
+      {
+        path: "blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: '/loader',
+        element:<LoadingSpinner></LoadingSpinner>
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
