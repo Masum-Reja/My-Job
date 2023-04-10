ab7427522cb8e10994d957e72084c54ef06938fe
Masum-Reja <masum15-13347@diu.edu.bd>
2023-04-10 21:40:27 +0600

    add banner

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MyImage from "../assets/All Images/P3OLGJ1 copy 1.png";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="md:flex items-center">
+        <div className=" md:pe-10">
+          <h1 className="text-6xl font-bold my-4 mt-20 lg:pe-24">
+            One Step Closer To Your{" "}
+            <span className=" text-indigo-600">Dream Job</span>
+          </h1>
+          <p className=" mb-5 text-slate-400 font-medium font-Manrope">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future. Come find it. Manage all your job application
+            from start to finish.
+          </p>
+          <Link className=" text-white bg-indigo-500/90 p-2 rounded-md font-bold">
+            Get Started
+          </Link>
+        </div>
+
+        <img width={"470px"} src={MyImage} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
 
- const loader=useLoaderData
+
 const Home = () => {
   return (
     <div>
-     <h1>Home</h1>
+              
+              <Banner></Banner>
     </div>
   );
 };
